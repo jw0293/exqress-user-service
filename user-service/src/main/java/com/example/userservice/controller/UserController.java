c9707557bb4a1b67764ae1efd5fa81e8695298ed
Kim jung won <k38836919@gmail.com>
2023-05-02 13:58:45 +0900

    Init User MicroService

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserServiceImpl;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final Greeting greeting;
+    private final UserServiceImpl userService;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/uesrs")
+    public ResponseEntity createUser(@RequestBody RequestUser user){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+}
