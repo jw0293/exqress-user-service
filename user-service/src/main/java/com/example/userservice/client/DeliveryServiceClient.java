c9ae913300ef53330d04c2fed578a40c160a8977
Kim jung won <k38836919@gmail.com>
2023-05-22 18:51:27 +0900

    remove OpenFeign

--- user-service/src/main/java/com/example/userservice/client/DeliveryServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/DeliveryServiceClient.java
@@ -1,12 +1,12 @@
-package com.example.userservice.client;
-
-import com.example.userservice.vo.response.ResponseParcel;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
-
+//package com.example.userservice.client;
+//
+//import com.example.userservice.vo.response.ResponseParcel;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//import java.util.List;
+//
 //@FeignClient(name = "delivery-service")
 //public interface DeliveryServiceClient {
 //
