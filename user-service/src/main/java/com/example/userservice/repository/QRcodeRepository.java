1a313a212b3ee9c950989df6f57fffaa6195406f
Kim jung won <k38836919@gmail.com>
2023-06-01 16:00:51 +0900

    refact : CrudRepo -> JpaRepo

--- user-service/src/main/java/com/example/userservice/repository/QRcodeRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/QRcodeRepository.java
@@ -1,9 +1,9 @@
 package com.example.userservice.repository;
 
 import com.example.userservice.entity.QRcode;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QRcodeRepository extends CrudRepository<QRcode, Long> {
+public interface QRcodeRepository extends JpaRepository<QRcode, Long> {
     QRcode findByInvoiceNo(String invoiceNo);
     QRcode findByQrId(String qrId);
 }
